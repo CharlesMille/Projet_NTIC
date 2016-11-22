fff4958636415bff45731a91420362d204d04658
Charles Mille <cmille@ensc.fr>
2016-11-22 17:28:42 +0100

    modification header

--- Website/php/header.php
+++ Website/php/header.php
@@ -1,7 +1,9 @@
-<header>
-    <div class = "h1 alignmiddle">
-        Le handicap psychique            
+<header id = "header"  class = "header">
+    <div class = "alignleft">
+        <div class="grid h1 m1 pr2"> Le handicap psychique </div>                           
+        <div class="grid h2 m1"> <a> Accueil </a> </div>
+        <div class="grid h2 m1"> <a> Lien 1 </a> </div>
+        <div class="grid h2 m1"> <a> Lien 2 </a> </div>
+        <div class="grid h2 m1"> <a> Lien 3 </a> </div>
     </div>
-    <br/>
-    <ul 
 </header>
