2d07ba66701f86d003150faed4c88f4ee4700d35
Alexandre Monge <amonge@ensc.fr>
2016-11-22 14:29:18 +0100

    Ajout site base

---
+++ Website/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+	<title>Projet NTIC</title>
+
+	<!-- IMPORT JS -->
+	<script type="text/javascript" src="js/fonctions.js"></script>
+	
+	<!-- Import CSS -->
+	<link type="text/css" rel="stylesheet" href="css/rocssti-fr.css"/>
+</head>
+<body>
+	
+</body>
+</html>
