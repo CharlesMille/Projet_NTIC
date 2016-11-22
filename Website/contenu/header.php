2ef1f6c125a5d5e7d74dcfffeb0cbca99bf676aa
Alexandre Monge <amonge@ensc.fr>
2016-11-22 17:41:47 +0100

    Header accessible

--- Website/contenu/header.php
+++ Website/contenu/header.php
@@ -1,59 +1,33 @@
-<header>
+<header id="header"  class="header">
 	<div class="skip m0 p0 noprint nomobile">
 		<a class="skip__link" href="#navigation">Navigation</a>
 		<span class="invisible"> | </span>
 		<a class="skip__link" href="#contenu">Contenu</a>
 	</div>
 
-	<div class="center mw1600e mb2">
-		<div class="header center mw1600e aligncenter relative">
-			<div class="h1">
-				<a class="logo">
-					<span>Le handicap psychique</span>
-				</a>
-			</div>
-		</div>
-
-		<div id="navigation" class="navigation center relative noprint">
-			<nav role="navigation">
-				<ul class="navigation row">
-					<li class="navigation__item col">
-						<div>
-							<a class="navigation__link zone" href="">
-								<span class="navigation__text">Test 1</span>
-							</a>
-						</div>
-					</li>
-					<li class="navigation__item col">
-						<div>
-							<a class="navigation__link zone" href="">
-								<span class="navigation__text">Test 2</span>
-							</a>
-						</div>
-					</li>
-					<li class="navigation__item col">
-						<div>
-							<a class="navigation__link zone" href="">
-								<span class="navigation__text">Test 3</span>
-							</a>
-						</div>
-					</li>
-					<li class="navigation__item col">
-						<div>
-							<a class="navigation__link zone" href="">
-								<span class="navigation__text">Test 4</span>
-							</a>
-						</div>
-					</li>
-					<li class="navigation__item col">
-						<div>
-							<a class="navigation__link zone" href="">
-								<span class="navigation__text">Test 5</span>
-							</a>
-						</div>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
+    <div id="navigation" class="navigation alignleft">
+        <div class="grid h1 m1 pr2">
+        	<span>Le handicap psychique</span>
+        </div>                           
+        <div class="grid h2 m1">
+        	<a class="navigation__link" href="">
+        		<span>Accueil</span>
+        	</a> 
+        </div>
+        <div class="grid h2 m1">
+        	<a class="navigation__link" href="">
+        		<span>Lien 1</span>
+        	</a> 
+        </div>
+        <div class="grid h2 m1">
+        	<a class="navigation__link" href="">
+        		<span>Lien 2</span>
+        	</a> 
+        </div>
+        <div class="grid h2 m1">
+        	<a class="navigation__link" href="">
+        		<span>Lien 3</span>
+        	</a> 
+        </div>
+    </div>
 </header>
