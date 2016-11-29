ed4ad3ba99a17c0489792d1d0bcbca63fa07ff23
Tubbbe <alexandre.monge@yahoo.fr>
2016-11-29 17:23:37 +0100

    Responsive header

--- Website/contenu/header.php
+++ Website/contenu/header.php
@@ -7,27 +7,56 @@
 
     <div id="navigation" class="navigation alignleft">
         <div class="grid h1 m1 pr2">
-        	<span>Le handicap psychique</span>
+            <span>Le handicap psychique</span>
+        	<img src="img/menu_icone.png" onclick="afficherTab();" id="boutonMenu" class="nodesktop" style="cursor: pointer;"></img>
         </div>                           
-        <div class="grid h2 m1">
+        <div class="grid h2 m1 notablet nomobile">
         	<a class="navigation__link" href="">
         		<span>Accueil</span>
         	</a> 
         </div>
-        <div class="grid h2 m1">
+        <div class="grid h2 m1 notablet nomobile">
         	<a class="navigation__link" href="">
         		<span>Lien 1</span>
         	</a> 
         </div>
-        <div class="grid h2 m1">
+        <div class="grid h2 m1 notablet nomobile">
         	<a class="navigation__link" href="">
         		<span>Lien 2</span>
         	</a> 
         </div>
-        <div class="grid h2 m1">
+        <div class="grid h2 m1 notablet nomobile">
         	<a class="navigation__link" href="">
         		<span>Lien 3</span>
         	</a> 
+        </div>
+
+        <div id="tab" class="invisible">
+            <div class="grid h2 m1 nodesktop">
+                <a class="navigation__link" href="">
+                    <span>Accueil</span>
+                </a> 
+                <br/>
+            </div>
+            <div class="grid h2 m1 nodesktop">
+                <a class="navigation__link" href="">
+                    <span>Lien 1</span>
+                </a> 
+                <br/>
+            </div>
+            <div class="grid h2 m1 nodesktop">
+                <a class="navigation__link" href="">
+                    <span>Lien 2</span>
+                </a> 
+                <br/>
+            </div>
+            <div class="grid h2 m1 nodesktop">
+                <a class="navigation__link" href="">
+                    <span>Lien 3</span>
+                </a> 
+                <br/>
+            </div>
+            <br/>
         </div>
     </div>
 </header>
---
+++ Website/js/fonctions.js
@@ -0,0 +1,7 @@
+function afficherTab() {
+
+	if (document.getElementById("tab").className == "invisible")
+		document.getElementById("tab").className = "";
+	else
+		document.getElementById("tab").className = "invisible";
+}
