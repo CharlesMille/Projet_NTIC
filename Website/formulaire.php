611a03643e9e0cea3ce888b81ee57a0d021f3e34
Alexandre Monge <amonge@ensc.fr>
2016-12-06 16:11:49 +0100

    Modif form

--- Website/formulaire.php
+++ Website/formulaire.php
@@ -8,30 +8,37 @@
 	<script type="text/javascript" src="js/fonctions.js"></script>
 	
 	<!-- Import CSS -->
+	<link type="text/css" rel="stylesheet" href="css/jquery-ui.css"/>
 	<link type="text/css" rel="stylesheet" href="css/rocssti-fr.css"/>
 	<link type="text/css" rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
 	<?php include("contenu/header.php"); ?>	
 	<div id="contenu" class="center mw1600e pl2 pr2 content">
-		<div class="main">
-			<h1 class="p1">Ajouter une nouvelle page :</h1>
-			<form method="POST" action="">
-				<label for="titre" class="row bl">
-					<span class="inbl w15 alignright pr1">Titre :</span>
-					<input class="w80" type="text" name="titre" id="titre" placeholder="Titre"/>
-				</label>
-				<label for="image" class="row bl">
-					<span class="inbl w15 alignright pr1">Image :</span>
-					<input class="w80" type="file" name="image" id="image"/>
-				</label>
-				<br/>
-				<label for="situation" class="row bl">
-					<span class="inbl w15 alignright pr1">Texte décrivant une situation :</span><br/>
+		
+		<h1 class="p1">Ajouter une nouvelle page :</h1>
+		<form method="POST" action="">
+			<div class="w100 mb1">
+				<div class="w100">
+				  	<span class="inbl alignright pr2">Titre</span>
+				  	<input class="w70" type="text" name="titre" id="titre"/>
+				</div>
+			</div>
+			<div class="w100 mb2">
+				<div class="grid w100">
+				  	<span class="inbl alignright pr1d5">Image</span>
+				  	<input class="w70" type="file" name="image" id="image"/>
+				</div>
+			</div>
+			<br/>
+			<div class="w100">
+				<div class="grid w100">
+					<span class="inbl w100">Texte décrivant une situation :</span><br/>
 					<textarea class="w100" tname="situation" id="situation"></textarea>
-				</label>
-			</form> 
-		</div>
+				</div>
+			</div>
+		</form> 
+		
 	</div>
 	<?php include("contenu/footer.php"); ?>
 </body>
