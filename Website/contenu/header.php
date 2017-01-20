7eea3259739aabd6b96fc1b7e64c1ab158ad7bd8
AlexMonge <alexandre.monge@yahoo.fr>
2017-01-20 13:46:58 +0100

    Modif boutons OK

--- Website/contenu/header.php
+++ Website/contenu/header.php
@@ -23,8 +23,8 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li <?php if ($nom == "index") echo 'class="active"'; ?>><a href="index.php">Accueil</a></li>
-        <li <?php if ($nom == "ajoutScenario") echo 'class="active"'; ?>><a href="ajoutScenario.php">Ajout d'un scénario</a></li>
         <li <?php if ($nom == "listeScenario") echo 'class="active"'; ?>><a href="listeScenario.php">Liste des scénarios</a></li>        
+        <li <?php if ($nom == "ajoutScenario") echo 'class="active"'; ?>><a href="ajoutScenario.php">Ajout d'un scénario</a></li>
       </ul>
 
       <ul class="nav navbar-nav navbar-right vcenter">
